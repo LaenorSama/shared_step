d6f370eef425a8e83b5e4ed8b1c94afdb39fb8af
LaenorSama <laenorsama@yandex.ru>
2025-06-09 21:03:30 +0500

    First

--- api_shared_step.py
+++ api_shared_step.py
@@ -1,20 +1,19 @@
 import requests
-import json
 import os
 # библиотека для загрузки данных из env
 from dotenv import load_dotenv
 
 
 # функция для получения bearer-токена нужного инстанса для токена администратора
-def get_bearer_token(TESTOPS_API_URL, TESTOPS_TOKEN):
+def get_bearer_token(testops_api_url, testops_token):
     # URL для получения токена
-    url = f"{TESTOPS_API_URL}uaa/oauth/token"
+    url = f"{testops_api_url}uaa/oauth/token"
 
     # Данные для запроса (в формате x-www-form-urlencoded)
     data = {
         "grant_type": "apitoken",
         "scope": "openid",
-        "token": TESTOPS_TOKEN
+        "token": testops_token
     }
 
     # Заголовки
@@ -39,28 +38,28 @@ def get_bearer_token(TESTOPS_API_URL, TESTOPS_TOKEN):
     else:
         print("Ошибка получения токена:", response.status_code, response.text)
 
-# функция получает название тест-кейса
+# функция получает название тест-кейса,
 # возвращает переменную типа текст
-def get_testcase_name(INSTANCE_NAME, TESTCASE_ID):
+def get_testcase_name(instance_name, testcase_id):
     headers = {
         "Authorization": f"Bearer {BEARER_TOKEN}",
         "Content-Type": "application/json"  # Для корректной обработки JSON
     }
-    response = requests.get(f"https://{INSTANCE_NAME}/api/testcase/{TESTCASE_ID}", headers=headers)
+    response = requests.get(f"https://{instance_name}/api/testcase/{testcase_id}", headers=headers)
     scenario_data = response.json()
     # Извлекаем имя
     scenario_name = scenario_data["name"]
     print(f'Название тест-кейса: {scenario_name}')
     return scenario_name
 
-# функция получается сценарий тест-кейса
+# функция получается сценарий тест-кейса,
 # возвращает список из шагов
-def get_testcase_scenario(INSTANCE_NAME, TESTCASE_ID):
+def get_testcase_scenario(instance_name, testcase_id):
     headers = {
         "Authorization": f"Bearer {BEARER_TOKEN}",
         "Content-Type": "application/json"  # Для корректной обработки JSON
     }
-    response = requests.get(f"https://{INSTANCE_NAME}/api/testcase/{TESTCASE_ID}/step", headers=headers)
+    response = requests.get(f"https://{instance_name}/api/testcase/{testcase_id}/step", headers=headers)
     scenario_data = response.json()
     # Извлекаем шаги
     children_ids = scenario_data["root"]["children"]
@@ -78,48 +77,48 @@ def get_testcase_scenario(INSTANCE_NAME, TESTCASE_ID):
 
     return steps
 
-# функция создает новый общий щаг
+# функция создает новый общий шаг,
 # возвращает ID общего шага
-def post_create_sharedstep(INSTANCE_NAME, PROJECT_ID, SHAREDSTEP_NAME):
+def post_create_sharedstep(instance_name, project_id, sharedstep_name):
     headers = {
         "Authorization": f"Bearer {BEARER_TOKEN}",
         "Content-Type": "application/json"  # Для корректной обработки JSON
     }
     # Формируем тело запроса
     payload = {
-        "projectId": PROJECT_ID,
-        "name": SHAREDSTEP_NAME,
+        "projectId": project_id,
+        "name": sharedstep_name,
         "archived": False
     }
-    response = requests.post(f"https://{INSTANCE_NAME}/api/sharedstep", headers=headers, json=payload)
+    response = requests.post(f"https://{instance_name}/api/sharedstep", headers=headers, json=payload)
     data = response.json()
     sharedstep_id = data["id"]
-    print(f'Создан общий щаг. ID = {sharedstep_id}')
+    print(f'Создан общий шаг. ID = {sharedstep_id}')
     return sharedstep_id
 
-# функция добавляет в общий шаг сценарий из списка шагов
+# функция добавляет в общий шаг сценарий из списка шагов,
 # ничего не возвращает
-def post_create_scenario_for_sharedstep(INSTANCE_NAME, SHAREDSTEP_ID, TESTCASE_SCENARIO):
+def post_create_scenario_for_sharedstep(instance_name, sharedstep_id, testcase_scenario):
     headers = {
         "Authorization": f"Bearer {BEARER_TOKEN}",
         "Content-Type": "application/json"  # Для корректной обработки JSON
     }
     print('Начинаем добавлять шаги сценария в общий шаг.')
-    for step in TESTCASE_SCENARIO:
+    for step in testcase_scenario:
         # Формируем тело запроса
         payload = {
-            "sharedStepId": SHAREDSTEP_ID,
+            "sharedStepId": sharedstep_id,
             "body": step
         }
         print(payload)
-        response = requests.post(f"https://{INSTANCE_NAME}/api/sharedstep/step", headers=headers, json=payload)
+        response = requests.post(f"https://{instance_name}/api/sharedstep/step", headers=headers, json=payload)
         print(f'статус ответа: {response.status_code}')
         print(f'Добавлен шаг: {step}')
     return
 
 # объявляем нужные переменные
-PROJECT_ID = 165
-TESTCASE_ID = 1820
+PROJECT_ID = 184
+TESTCASE_ID = 1830
 INSTANCE_NAME = "demo.qatools.cloud"
 
 # Загружаем .env файл
@@ -129,10 +128,12 @@ TESTOPS_TOKEN = os.getenv("TESTOPS_TOKEN")
 BEARER_TOKEN = get_bearer_token(f"https://{INSTANCE_NAME}/api/", TESTOPS_TOKEN)
 
 # получаем имя тест-кейса
-testcase_name = get_testcase_name(INSTANCE_NAME, TESTCASE_ID)
+TESTCASE_NAME = get_testcase_name(INSTANCE_NAME, TESTCASE_ID)
 # получаем сценарий тест-кейса
-testcase_scenario = get_testcase_scenario(INSTANCE_NAME, TESTCASE_ID)
+TESTCASE_SCENARIO = get_testcase_scenario(INSTANCE_NAME, TESTCASE_ID)
 # создаем общий шаг
-sharedstep_id = post_create_sharedstep(INSTANCE_NAME, PROJECT_ID, testcase_name)
+SHAREDSTEP_ID = post_create_sharedstep(INSTANCE_NAME, PROJECT_ID, TESTCASE_NAME)
 # добавляем сценарий из списка шагов в общий шаг
-post_create_scenario_for_sharedstep(INSTANCE_NAME, sharedstep_id, testcase_scenario)
+post_create_scenario_for_sharedstep(INSTANCE_NAME, SHAREDSTEP_ID, TESTCASE_SCENARIO)
+
+
